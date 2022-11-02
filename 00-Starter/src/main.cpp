f014fc63058a9e4c273f9eb032ddde12fca3c13c
Alexis Haldy <alexishaldy@gmail.com>
2022-11-02 17:51:52 +0100

    Added circle shape spawner

--- 00-Starter/src/main.cpp
+++ 00-Starter/src/main.cpp
@@ -2,37 +2,103 @@
 #include "box2d/b2_world.h"
 #include "box2d/b2_polygon_shape.h"
 #include "box2d/b2_fixture.h"
+#include "box2d/b2_circle_shape.h"
+
+#include "SFML/Graphics.hpp"
+#include "Random.h"
+
+constexpr float SCALE = 10.f;
+constexpr int WIDTH = 800;
+constexpr int HEIGHT = 600;
+
+// Convert Box2D coordinates to SFML coordinates
+sf::Vector2f b2ToSf(const b2Vec2& v)
+{
+	return sf::Vector2f(v.x * SCALE, HEIGHT - (v.y * SCALE));
+}
+
+// Convert SFML coordinates to Box2D coordinates
+b2Vec2 sfToB2(const sf::Vector2f& v)
+{
+	return b2Vec2(v.x / SCALE, (HEIGHT - v.y) / SCALE);
+}
 
 int main()
 {
-	const b2Vec2 gravity(0.0f, 9.8f);
+	std::vector<b2Body*> bodies;
+
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Box2D");
+
+	window.setFramerateLimit(144);
+	window.setVerticalSyncEnabled(true);
+
+	b2Vec2 gravity(0.0f, -30.f);
 	b2World world(gravity);
 
-	// Creating the floor
-	b2BodyDef floorDef;
-	floorDef.position.Set(0.0f, -10.0f);
-	b2Body* floor = world.CreateBody(&floorDef);
+	b2BodyDef groundBodyDef;
+	groundBodyDef.position.Set(0.0f, 3.0f);
+	b2Body* groundBody = world.CreateBody(&groundBodyDef);
 
-	b2PolygonShape floorShape;
-	floorShape.SetAsBox(50.0f, 10.0f);
-	b2FixtureDef floorFixtureDef;
-	floorFixtureDef.shape = &floorShape;
-	floor->CreateFixture(&floorFixtureDef);
+	b2PolygonShape groundBox;
+	groundBox.SetAsBox(WIDTH / SCALE, groundBodyDef.position.y);
+	groundBody->CreateFixture(&groundBox, 2.0f);
 
-	// Creating the box
-	b2BodyDef boxDef;
-	boxDef.type = b2_dynamicBody;
-	boxDef.position.Set(0.0f, 4.0f);
-	b2Body* box = world.CreateBody(&boxDef);
+	sf::RectangleShape ground(b2ToSf(groundBox.m_vertices[2]) - b2ToSf(groundBox.m_vertices[0]));
+	ground.setOrigin(ground.getSize() / 2.0f);
+	ground.setFillColor(sf::Color::Red);
+	ground.setPosition(b2ToSf(groundBody->GetPosition()));
 
-	b2PolygonShape boxShape;
-	boxShape.SetAsBox(1.0f, 1.0f);
-	b2FixtureDef boxFixtureDef;
-	boxFixtureDef.shape = &boxShape;
-	boxFixtureDef.density = 1.0f;
-	boxFixtureDef.friction = 0.3f;
-	boxFixtureDef.restitution = 0.5f;
-	box->CreateFixture(&boxFixtureDef);
+	while (window.isOpen())
+	{
+		sf::Event event;
+
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+		{
+			b2BodyDef bodyDef;
+			bodyDef.type = b2_dynamicBody;
+			bodyDef.position = sfToB2(sf::Vector2f(sf::Mouse::getPosition(window)));
+			b2Body* body = world.CreateBody(&bodyDef);
+
+			b2CircleShape dynamicBox;
+			dynamicBox.m_radius = Random::GetFloat(0.5f, 2.0f);
+			b2FixtureDef fixtureDef;
+			fixtureDef.shape = &dynamicBox;
+			fixtureDef.density = Random::GetFloat(10.f, 5000.f);
+			fixtureDef.friction = Random::GetFloat(0.1f, 1.f);
+			body->CreateFixture(&fixtureDef);
+
+			bodies.push_back(body);
+		}
+
+		world.Step(1.0f / 60.0f, 8, 3);
+
+		window.clear();
+		window.draw(ground);
+
+		for (auto body : bodies)
+		{
+			// Transform body into sfml rectangle shape and draw it
+			b2Fixture* fixture = body->GetFixtureList();
+			b2CircleShape* shape = static_cast<b2CircleShape*>(fixture->GetShape());
+
+			sf::CircleShape circle(shape->m_radius * SCALE);
+			circle.setOrigin(circle.getRadius(), circle.getRadius());
+			circle.setFillColor(sf::Color::Green);
+			circle.setPosition(b2ToSf(body->GetPosition()));
+			circle.setOutlineColor(sf::Color::Black);
+			circle.setOutlineThickness(-1.f);
+
+			window.draw(circle);
+		}
+
+		window.display();
+	}
 
 	return 0;
 }
